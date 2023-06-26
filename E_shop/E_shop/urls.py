5044d1d213ff711c645cd2647ed93ded11df550d
Killol14 <kgselectrical.plumbing@gmail.com>
2023-06-26 12:11:16 +0000

    trying to make base.html work

--- E_shop/E_shop/urls.py
+++ E_shop/E_shop/urls.py
@@ -20,5 +20,6 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.HOME,name='home'),
     path('base/', views.BASE,name='base'),
 ]
--- E_shop/E_shop/views.py
+++ E_shop/E_shop/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render,redirect
 
 def BASE(request):
     return render(request, 'main/base.html')
+
+def HOME(request):
+    return render(request, 'main/index.html')    
